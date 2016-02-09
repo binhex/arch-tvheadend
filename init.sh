7bc882308b75a35482107249cd1f8edc8e934663
binhex <megalith01@gmail.com>
2016-02-09 22:51:03 +0000

    set perms on /config, set tvh not to restart (forks)

--- init.sh
+++ init.sh
@@ -12,10 +12,17 @@ fi
 
 echo "[info] User ID defined as ${UID}"
 echo "[info] Group ID defined as ${GID}"
+echo "[info] Setting permissions..."
 
-# set permissions for files/folders
+# set permissions for /config volume mapping
+chown -R $UID:$GID /config
+chmod -R 775 /config
+
+# set permissions inside container
 chown -R $UID:$GID /usr/bin/tvheadend /run
 chmod -R 775 /usr/bin/tvheadend /run
+
+echo "[info] Starting Supervisor..."
 
 # run supervisor
 "/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
