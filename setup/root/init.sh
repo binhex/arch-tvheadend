92a1a577edf17425b4c760190878dffe66eb38cb
binhex <megalith01@gmail.com>
2016-09-11 22:28:37 +0100

    config supervisor for pid 1

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775 /usr/bin/tvheadend /run /home/nobody
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
