de7574fc7dee2be39630afacb63f1fc5e141c37f
binhex <megalith01@gmail.com>
2021-10-13 10:37:09 +0100

    put in arg for release tag name

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,6 +3,9 @@
 # exit script if return code != 0
 set -e
 
+# release tag name from build arg, stripped of build ver using string manipulation
+release_tag_name="${1//-[0-9][0-9]/}"
+
 # build scripts
 ####
 
@@ -45,7 +48,7 @@ fi
 # aur packages
 ####
 
-# need to install this separately, as its now a requirement of 
+# need to install this separately, as its now a requirement of
 # package 'tvheadned' which fails unless this is already installed
 aur_packages="libavresample"
 
@@ -61,7 +64,7 @@ source aur.sh
 # container perms
 ####
 
-# define comma separated list of paths 
+# define comma separated list of paths
 install_paths="/run,/home/nobody"
 
 # split comma separated string into list for install paths
@@ -91,7 +94,7 @@ cat <<EOF > /tmp/permissions_heredoc
 previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
 previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
 
-# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different 
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
 # from the previous run then re-apply chown with current PUID and PGID values.
 if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
 
